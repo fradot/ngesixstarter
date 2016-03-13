3aa5ee6062603087d6790ad4abc4c6057c836e44
Francesco Tucceri <francesco.tucceri@gmail.com>
2016-03-13 16:25:57 +0100

    Update: removed unneded comment

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,6 @@ module.exports = function(config) {
 
     colors: true,
 
-    // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO
 
